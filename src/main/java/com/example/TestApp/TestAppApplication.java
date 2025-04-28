515c7437870c08eb458e58b39366facb678166ce
Kacper Michalak <michalakkacper2002@gmail.com>
2025-04-28 19:10:27 +0200

    Simple use of configuration properties

--- src/main/java/com/example/TestApp/TestAppApplication.java
+++ src/main/java/com/example/TestApp/TestAppApplication.java
@@ -2,6 +2,7 @@ package com.example.TestApp;
 
 import com.example.TestApp.services.ColourPrinter;
 import com.example.TestApp.services.impl.ColourPrinterImpl;
+import com.example.TestApp.services.impl.PizzaConfig;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestAppApplication implements CommandLineRunner {
 
-	private ColourPrinter colourPrinter;
+	//private ColourPrinter colourPrinter;
+	private PizzaConfig pizzaConfig;
 
-	public TestAppApplication(ColourPrinter colourPrinter){
-		this.colourPrinter = colourPrinter;
+	public TestAppApplication(PizzaConfig pizzaConfig){
+		this.pizzaConfig = pizzaConfig;
 
 	}
 
@@ -22,6 +24,9 @@ public class TestAppApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(colourPrinter.print());
+		System.out.println(String.format("I want a %s crust pizza, with %s and %s sauce",
+				pizzaConfig.getCrust(),
+				pizzaConfig.getTopping(),
+				pizzaConfig.getSauce()));
 	}
 }
